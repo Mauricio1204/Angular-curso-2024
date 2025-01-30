d8017c58ad36b35179fc7c53304ea89848d4d826
Mauricio Chavez Mejia <mauchavezmeji34@gmail.com>
2025-01-29 22:03:37 -0600

    29-21-2025

--- 03-gifs-app/src/app/gifs/components/saerch-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/saerch-box/search-box.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
   <h5>Buscar:</h5>
   <input
-  type="text"
+  hola type="text"
   class="form-control"
-  placeholder="Search for gifs...">`
+  placeholder="Search for gifs..."
+  (keyup.enter)="serchTag()"
+  #txtTagImput
+  >
+  `
 })
 
 export class SearchBoxComponent  {
+  @ViewChild('txtTagImput')
+ public tagImput !:ElementRef<HTMLInputElement>;
+
+
+
   constructor() { }
+
+
+  serchTag(){
+    const newTag =this.tagImput.nativeElement.value
+    console.log( newTag);
+  }
 
 }
---
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+
+  private _tagsHistory: string[] = [];
+
+  constructor() {  }
+
+
+  get tagsHistory(): string[] {
+    return [...this._tagsHistory]
+  }
+
+}
